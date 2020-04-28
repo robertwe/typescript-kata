7dcaa0cd9d154c0277262d5c1432575174c0165e
Robert Weclawski <robert.weclawski@mykolab.com>
2020-04-28 23:13:06 +0200

    Pkgs upgrade

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,5 +1,15 @@
-describe("KataPokerHands", () => {
-  it("Fails", () => {
-    throw "NOT IMPLEMENTED"
-  });
-})
+// describe("KataPokerHands", () => {
+//   it("Fails", () => {
+//     throw "NOT IMPLEMENTED"
+//   });
+// })
+
+describe("Kata", () => {
+    test('adds 1 + 2 to equal 3', () => {
+      expect(1 + 2).toBe(3);
+    });
+    test('adds 1 + 2 to equal 3', () => {
+      expect(true).toBe(true);
+    });
+
+});
